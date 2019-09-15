cc344ea1165e4bb5257b0d35ad621c5e7bfa3752
Nidhi Trapasiya <51698145+NidhiTrapasiya@users.noreply.github.com>
2019-09-15 10:11:25 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,9 @@
+# Python code to illustrate cube of a number  
+# showing difference between def() and lambda(). 
+def cube(y): 
+    return y*y*y; 
+  
+g = lambda x: x*x*x 
+print(g(7)) 
+  
+print(cube(5))
